a5212c3115c9a0af7f350cccbc51d5a8bd5b2b0c
Alexey Slesarev <lexsl84@gmail.com>
2017-05-24 10:28:17 +0000

    i'm close to solution

--- module1/styles/app.js
+++ module1/styles/app.js
@@ -4,12 +4,29 @@
 
   LunchCheckController.$inject = ['$scope'];
   function LunchCheckController($scope) {
-    $scope.name = "";
+   $scope.items = "";
+   var good_variant = $scope.items.split([/(\d+)?[.,]?(\d+)\./['']]);
+   var bad_variant = $scope.items.split([/(\d+)?[.,]?(\d+)\./['', 4]]);
+   $scope.clickButton = function() {
+    if ($scope.items.split([/(\d+)?[.,]?(\d+)\./['']]).length == ""){
+      $scope.text = "empty";
+    }else if ($scope.items.split("").length == good_variant.length){
+      $scope.text = "good";
+    }else if($scope.items.split("") == bad_variant){
+      $scope.text = "bad";
+    }
+    
+    
+   }
+   
+   
+   
+   
+  
+   
   };
   
-  function ClickButton($scope) {
-    $scope.name = "Hello!";
-  };
+
 
 
 })();
