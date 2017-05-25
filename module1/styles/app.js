ba20836a76ca13a1353abc4203404ce2c29964cc
Alexey Slesarev <lexsl84@gmail.com>
2017-05-25 08:22:11 +0000

    some bonus

--- module1/styles/app.js
+++ module1/styles/app.js
@@ -8,10 +8,16 @@
    $scope.clickButton = function() {
     if ($scope.items.split("").length == "0"){
       $scope.text = "Please enter data first.";
+      $scope.my_style = {color:'red'};
+      $scope.my_style2 = {border: '1px solid red'};
     }else if ($scope.items.split(" ").length >= 4 ){
       $scope.text = "Too much!";
+      $scope.my_style = {color:'green'};
+      $scope.my_style2 = {border: '1px solid green'};
     }else if($scope.items.split(" ").length <= 3 ) {
       $scope.text = "Enjoy!";
+      $scope.my_style = {color:'green'};
+      $scope.my_style2 = {border: '1px solid green'};
     }
    };
   }
