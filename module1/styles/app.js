f54aeed0a50fb40fa778dcdd445bb8e6c9013ab4
Alexey Slesarev <lexsl84@gmail.com>
2017-05-26 06:15:03 +0700

    solve my problem

--- module1/styles/app.js
+++ module1/styles/app.js
@@ -10,11 +10,11 @@
       $scope.text = "Please enter data first.";
       $scope.my_style = {color:'red'};
       $scope.my_style2 = {border: '1px solid red'};
-    }else if ($scope.items.split(" ").length >= 4 ){
+    }else if ($scope.items.split(",").length >= 4 ){
       $scope.text = "Too much!";
       $scope.my_style = {color:'green'};
       $scope.my_style2 = {border: '1px solid green'};
-    }else if($scope.items.split(" ").length <= 3 ) {
+    }else if($scope.items.split(",").length <= 3 ) {
       $scope.text = "Enjoy!";
       $scope.my_style = {color:'green'};
       $scope.my_style2 = {border: '1px solid green'};
